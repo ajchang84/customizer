5c7f66b212c87a401a0818dd7779ae1a063accb3
Alexander Chang <gxchang@gmail.com>
2019-10-16 17:00:22 +0800

    test

--- src/index.js
+++ src/index.js
@@ -102,26 +102,30 @@ app.get("/api/getRepo", (req, res) => {
     }
 });
 
-app.get("/api/download", (req, res)=>{
-    console.log('test')
-    res.redirect(req.originalUrl)
-}), (req, res) => {
-    let project = req.query.project || "bac";
-    const currentdate = new Date();
-    const datetime =
-        project +
-        "_" +
-        currentdate.getFullYear() +
-        (currentdate.getMonth() + 1) +
-        currentdate.getDate() +
-        "_" +
-        currentdate.getHours() +
-        currentdate.getMinutes();
-    execSync(`zip -r ${datetime} *`, {
-        cwd: DOWNLOAD_PATH
-    });
-    res.download(path.join(DOWNLOAD_PATH, `/${datetime}.zip`));
-});
+app.get(
+    "/api/download",
+    (req, res) => {
+        console.log("test");
+        res.redirect(req.originalUrl);
+    },
+    (req, res) => {
+        let project = req.query.project || "bac";
+        const currentdate = new Date();
+        const datetime =
+            project +
+            "_" +
+            currentdate.getFullYear() +
+            (currentdate.getMonth() + 1) +
+            currentdate.getDate() +
+            "_" +
+            currentdate.getHours() +
+            currentdate.getMinutes();
+        execSync(`zip -r ${datetime} *`, {
+            cwd: DOWNLOAD_PATH
+        });
+        res.download(path.join(DOWNLOAD_PATH, `/${datetime}.zip`));
+    }
+);
 
 const TEXTURE_FOLDERS = {
     bac: "BAC",
